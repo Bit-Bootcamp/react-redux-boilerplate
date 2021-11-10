9c437346ed72fd326902cede7a895250a33884d7
Hama Dler <hamadler48@gmail.com>
2021-11-10 20:37:25 +0300

    chore: nav brand text

--- src/components/TheNavbar.js
+++ src/components/TheNavbar.js
@@ -8,7 +8,7 @@ export default function TheNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">React Redux Boilerplate</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
